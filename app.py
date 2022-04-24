fb9e5c0c1a94f72b3e951fcf1e499c666a4412e8
Arya R N <63279825+ranilakshmi@users.noreply.github.com>
2022-04-24 17:47:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ y_preds = tf.argmax(pred_probs, axis=1)
 class_names = ['business', 'editorial', 'entertainment', 'nation', 'sports']
 if st.button('Predict'):
     st.write(f"Predicted label: {class_names[y_preds[[0]]]}\n")
+st.heading('NLP Project')
+st.write('Team Members')
+st.write('1.Sujanya Reddy')
+st.write('2.Deepthi Appasani')
+st.write('3.Arya R N')
+st.write('4.Pannaga Sreya')
