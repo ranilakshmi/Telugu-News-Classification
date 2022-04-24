a99acffeab8f8663434a805d43cfd22f46fbc424
Arya R N <63279825+ranilakshmi@users.noreply.github.com>
2022-04-24 17:55:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import tensorflow as tf
 st.title("Telugu News Classification")
-st.subheader("NLP Project")
-st.write("Team Members")
-st.write("1.Sujanya Reddy")
-st.write("2.Deepthi Appasani")
-st.write("3.Arya R N")
-st.write("4.Pannaga Sreya")
-txt = st.text_area("News article")
+txt = st.text_area("Enter the News article")
 model = tf.keras.models.load_model('model')
 pred_probs = model.predict([txt])
 y_preds = tf.argmax(pred_probs, axis=1)
 class_names = ['business', 'editorial', 'entertainment', 'nation', 'sports']
 if st.button('Predict'):
     st.write(f"Predicted label: {class_names[y_preds[[0]]]}\n")
-
-    
+st.subheader("NLP Project")
+st.write("Team Members")
+st.write("1.Sujanya Reddy")
+st.write("2.Deepthi Appasani")
+st.write("3.Arya R N")
+st.write("4.Pannaga Sreya")
